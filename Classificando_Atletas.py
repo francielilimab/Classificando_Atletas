44040883820793d2201c6fd8d49cf98f01acc37b
Francieli <francielilimab@gmail.com>
2021-12-08 17:09:13 -0400

    código atualizado

---
+++ Classificando_Atletas.py
@@ -0,0 +1,31 @@
+from datetime import date
+from time import sleep
+print('Verificando a categoria de atletas...')
+ano = date.today().year
+while True:
+    nascimento = int(input('Digite o ano de nascimento: '))
+    idade = ano - nascimento
+    print(f'O atleta tem {ano - nascimento} anos.')
+    if idade <= 9:
+        print('Está na categoria \033[1MMIRIM!\033[m')
+    elif idade <= 14:
+        print('Está na categoria \33[1mINFANTIL!\033[m')
+    elif idade <= 19:
+        print('Está na categoria \033[1mJUNIOR!\033[m')
+    elif idade <= 25:
+        print('Está na categoria \033[1mSÊNIOR!\033[m')
+    else:
+        print('Está na categoria \033[1mMASTER!\033[m')
+    while True:
+        print('-' * 40)
+        resp = str(input('Quer continuar? (S/N) ')).strip().upper()[0]
+        if resp in 'SN':
+            break
+        else:
+            print('Por favor digite apenas S ou N.')
+    if resp in 'N':
+        print('Finalizando...')
+        sleep(1)
+        break
+print('='*40)
+print('Programa Finalizado')
